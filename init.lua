26bf77419508f3318d90a5ac986fb27aa7864d7a
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2023-10-11 22:25:39 +0200

    Remove weird treesitter bug regarding help texts

--- init.lua
+++ init.lua
@@ -196,6 +196,7 @@ require('lazy').setup({
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    opts = { ignore_install = { 'help' } },
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
@@ -336,7 +337,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'vim' },
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
 
